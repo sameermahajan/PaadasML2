97434211c2d4761b38cf813781fbf79e6e9673fd
Sameer Mahajan <GS-1316@GSLAB.COM>
2023-07-01 17:00:17 +0530

    Add trained model for 40 marathi numbers

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 from keras import models
 import numpy as np
-import sys
 
+class_to_number = {0 : 1, 1 : 10, 2 : 11, 3 : 12, 4 : 13, 5 : 14, 6 : 15, 7 : 16, 8 : 17, 9 : 18, 10 : 19,
+	          11 : 2, 12 : 21, 13 : 22, 14 : 23, 15 : 24, 16 : 25, 17 : 26, 18 : 27, 19 : 28, 20 : 29}
+                    
 def get_spectrogram(waveform):
   # Convert the waveform to a spectrogram via a STFT.
   spectrogram = tf.signal.stft(
@@ -15,15 +17,15 @@ def get_spectrogram(waveform):
   spectrogram = spectrogram[..., tf.newaxis]
   return spectrogram
   
-model = tf.keras.models.load_model("../ML-Audio-Models/tensorflow/marathi-40")
+model = tf.keras.models.load_model("marathi-40")
 #model.summary()
 
-x = tf.io.read_file(sys.argv[1])
+x = tf.io.read_file('1_0.wav')
 x, sample_rate = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000,)
 x = tf.squeeze(x, axis=-1)
 waveform = x
 x = get_spectrogram(x)
 x = x[tf.newaxis,...]
 prediction = model(x)
-#print (prediction)
-print (np.argmax(prediction))
+cls = np.argmax(prediction)
+print (class_to_number[cls])
