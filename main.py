ffd032f9e0f405263f23cc635c99d38331a030b2
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2023-07-01 10:57:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from keras import models
+import numpy as np
+import sys
+
+def get_spectrogram(waveform):
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      waveform, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+  
+model = tf.keras.models.load_model("../ML-Audio-Models/tensorflow/marathi-40")
+#model.summary()
+
+x = tf.io.read_file(sys.argv[1])
+x, sample_rate = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000,)
+x = tf.squeeze(x, axis=-1)
+waveform = x
+x = get_spectrogram(x)
+x = x[tf.newaxis,...]
+prediction = model(x)
+#print (prediction)
+print (np.argmax(prediction))
